a380a962d79be753680ed7f727370f4a056c603f
Barış SAVAŞ <barissavas17@gmail.com>
2023-06-19 21:37:07 +0300

    edit feature added

--- note-taking-app/src/EditNote.tsx
+++ note-taking-app/src/EditNote.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import NoteForm from './NoteForm'
-import { NoteEditP, NoteFormP } from './Types'
+import { NoteEditP} from './Types'
 import { useNote } from './NoteLayout'
 
 function EditNote({ onSubmit, onAddTag, availableTags }: NoteEditP) {
