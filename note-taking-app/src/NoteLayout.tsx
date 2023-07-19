603b12d3e974ca7b440345297e7983702825fb92
Barış SAVAŞ <barissavas17@gmail.com>
2023-07-19 18:48:08 +0300

    component added

--- note-taking-app/src/NoteLayout.tsx
+++ note-taking-app/src/NoteLayout.tsx
@@ -1,20 +1,18 @@
-import React from 'react'
 import { Note, NoteLayoutP } from './Types'
 import { Navigate, Outlet, useOutletContext, useParams } from 'react-router-dom'
 
+function NoteLayout({ notes }: NoteLayoutP) {
+  console.log(notes)
+  const { id } = useParams();
+  const note = notes.find(n => n.id === id);
 
+  if (note == null) return <Navigate to="/" replace />
 
-function NoteLayout({notes} : NoteLayoutP) {
-    const { id }= useParams();
-    const note = notes.find(n => n.id === id);
-    
-    if ( note == null) return <Navigate to="/" replace />
-    
   return <Outlet context={note} />
 }
 
 export default NoteLayout
 
 export function useNote() {
-    return useOutletContext<Note>()
+  return useOutletContext<Note>()
 }
