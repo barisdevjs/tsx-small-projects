6846aca4f7810cb1438bbb39a0cd4c56fcc1d10a
Barış SAVAŞ <barissavas17@gmail.com>
2024-02-08 22:56:26 +0300

    feat: singleProduct page design started

--- redux-sample/src/components/SingleProduct.tsx
+++ redux-sample/src/components/SingleProduct.tsx
@@ -1,10 +1,68 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchProductsById } from "../slices/eCommerceSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store";
+import { Button, Card, Descriptions, Image } from "antd";
+import type { DescriptionsProps } from "antd";
+import { IECommerce } from "../types/generalTypes";
+import { capitalizeFirstLetter } from "../utils/helpers";
+
+const { Meta } = Card;
 
 export default function SingleProduct() {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const product = useSelector(
+    (state: RootState) => state.productsReducer.singleProduct
+  );
+  const keyValuesArray: DescriptionsProps["items"] = Object.keys(product)
+    .filter((key) => key !== "rating")
+    .map((key, i) => ({
+      key: i,
+      label: capitalizeFirstLetter(key),
+      children:
+        key === "image" ? (
+          <Image width={200} src={product[key]} />
+        ) : (
+          product[key as keyof Omit<IECommerce, "rating">]
+        ),
+    }));
+
+  const status =
+    useSelector((state: RootState) => state.productsReducer.status) ===
+    "loading"
+      ? true
+      : false;
+
+  const descriptionItem = keyValuesArray.find(
+    (item) => item.label === "Description"
+  );
+  const description = descriptionItem ? descriptionItem.children : "";
+
+  const titleItem = keyValuesArray.find((item) => item.label === "Title");
+  const title = titleItem ? titleItem.children : "";
+
+  const imageItem = keyValuesArray.find((item) => item.label === "Image");
+  const image = imageItem
+    ? (imageItem.children as React.ReactElement)?.props.src
+    : "";
+
+  useEffect(() => {
+    dispatch(fetchProductsById(id as string));
+  }, [dispatch, id]);
   return (
     <div>
-      Sample Page ID ==`{">"}`{id}
+      {status && <div>Loading...</div>}
+      {!status && (
+        <Card
+          style={{ width: "20rem" }}
+          cover={<img alt="example" src={image} />}
+        >
+          <Meta title={title} description={description} />
+        </Card>
+      )}
     </div>
   );
 }
--- redux-sample/src/slices/eCommerceSlice.ts
+++ redux-sample/src/slices/eCommerceSlice.ts
@@ -17,9 +17,22 @@ import { IECommerce } from "../types/generalTypes";
 const initialState: {
   products: IECommerce[];
   status: "idle" | "loading" | "failed";
+  singleProduct: IECommerce;
 } = {
   products: [],
   status: "idle",
+  singleProduct: {
+    id: 0,
+    title: "",
+    price: 0,
+    description: "",
+    category: "",
+    image: "",
+    rating: {
+      rate: 0,
+      count: 0,
+    },
+  },
 };
 
 const productsSlice = createSlice({
@@ -38,6 +51,18 @@ const productsSlice = createSlice({
       .addCase(fetchProducts.rejected, (state) => {
         state.status = "failed";
       });
+
+    builder
+      .addCase(fetchProductsById.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProductsById.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.singleProduct = action.payload;
+      })
+      .addCase(fetchProductsById.rejected, (state) => {
+        state.status = "failed";
+      });
   },
 });
 
@@ -48,6 +73,20 @@ export const fetchProducts = createAsyncThunk(
     if (!response.ok) {
       throw new Error("Failed to fetch products");
     }
+    const data = await response.json();
+    return data;
+  }
+);
+
+export const fetchProductsById = createAsyncThunk(
+  "products/fetchProductsById",
+  async (id: string) => {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch product with ID: ${id}`);
+    }
+    // artifical delay for loading state
+    await new Promise((resolve) => setTimeout(resolve, 300));
     const data = await response.json();
     return data;
   }
